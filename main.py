567499840836e18f7be29d25c41939a7f563147e
Lucas Bubner <lkbubner@proton.me>
2022-11-15 18:25:05 +1030

    Added password authentication (needs password) to allow editing articles

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
 # Lucas Bubner, 2022
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
+logged_in = False
+
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", logged_in=logged_in)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    global logged_in
+    if request.method == 'POST':
+        password = request.form.get("password")
+
+        if password == "test":
+            logged_in = True
+            return redirect("/")
+
+        return render_template("login.html", incorrect=True)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    global logged_in
+    logged_in = False
+    return redirect("/")
+
 
 if __name__ == '__main__':
     app.run("0.0.0.0", debug=True)
