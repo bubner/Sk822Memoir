8bfadf7356df37b3a477d5d77343ed18cd909fcf
Lucas Bubner <81782264+holo-lb@users.noreply.github.com>
2022-11-15 04:33:46 +0000

    Added base backbone for website

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+# Lucas Bubner, 2022
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run("0.0.0.0", debug=True)
